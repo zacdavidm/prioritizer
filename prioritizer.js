f5e101727ece32a7140e1e633e8300c07711afb4
ZacDavidM <zacdavidm@gmail.com>
2015-08-24 22:46:36 -0400

    add support for variables, localStorage, sessonStorage, and inputs as data-prioritize keys

--- prioritizer.js
+++ prioritizer.js
@@ -14,14 +14,37 @@ var getUrlParameter = function getUrlParameter(sParam) {
 };
 
 $("[data-prioritize]").each(function(index, element){
-  console.log(element);
+  console.log($(element));
+
   //console.log($(element).data('prioritize'));
-  var key=$(element).data('prioritize');
-  var get_value=getUrlParameter(key);
+  //console.log($(element).data('prioritize'));
+  data_prioritize=$(element).data('prioritize');
+  var method=data_prioritize.method;
+  var key=data_prioritize.key;
+  var value;
+  if(method=="GET"){
+    value=getUrlParameter(key);
+  }else if(method=="localStorage"){
+    if(typeof(Storage) !== "undefined") {
+      value=localStorage[key];
+    }
+  }else if(method=="sessionStorage"){
+    if(typeof(Storage) !== "undefined") {
+      value=sessionStorage[key];
+    }
+  }else if(method=="input"){
+    value=$([name=key]).val();
+  }else if(method=="variable"){
+    //console.log(key);
+    //console.log(window[key]);
+    value=window[key];
+  }
+
+  var append=[];
   $(element).children('[data-prioritize-watch]').each(function (i, watch_element) {
     console.log($(watch_element).data('prioritize-watch'));
     data_value=$(watch_element).data('prioritize-watch');
-    if(get_value==data_value){
+    if(value==data_value){
       $(element).prepend($(watch_element).detach());
     }
   });
